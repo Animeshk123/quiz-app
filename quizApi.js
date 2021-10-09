25d30bc0c6992035b72ec23a79e152300bf3de69
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2021-10-09 18:56:55 +0530

    Add files via upload

---
+++ quizApi.js
@@ -0,0 +1,90 @@
+const quizApi = [
+    {
+        id: 1,
+        question: "What does CSS stand for ?",
+        hints: {
+            hint1: "Creative Style Sheets",
+            hint2: "Colorful Style Sheets",
+            hint3: "Computer Style Sheets",
+            hint4: "Cascading Style Sheets"
+        },
+        Ans: "Cascading Style Sheets"
+    },
+    {
+        id: 2,
+        question: "Where in an HTML document is the correct place of refer to an external style sheet ?",
+        hints: {
+            hint1: "in the <head>  Section",
+            hint2: "in the <body> Section",
+            hint3: "n the <script> Section",
+            hint4: "At the end of the document"
+        },
+        Ans: "in the <head> Section"
+        },
+    {
+        id: 3,
+        question: "Which HTML tag is used to define an internal style sheet ?",
+        hints: {
+            hint1: "<css>",
+            hint2: "<style>",
+            hint3: "<script>",
+            hint4: "<link>"
+        },
+        Ans: "<style>"
+            },
+    {
+        id: 4,
+        question: "Which HTML attribute is used to define inline styles ?",
+        hints: {
+            hint1: "font",
+            hint2: "styles",
+            hint3: "class",
+            hint4: "style"
+        },
+        Ans: "style"
+                },
+    {
+        id: 5,
+        question: "Which is the correct CSS syntax ?",
+        hints: {
+            hint1: "body:color=black;",
+            hint2: "body {color: black;}",
+            hint3: "{body;color:black;}",
+            hint4: "{body:color=black;}"
+        },
+        Ans: "body {color: black;}"
+                    },
+      {
+        id:6,
+        question:"Inside which HTML element do we put the JavaScript?",
+        hints : {
+          hint1:"<js>",
+          hint2:"<script>",
+          hint3:"<JavaScript>",
+          hint4:"<scripiting>"
+        },
+        Ans:"<script>"
+      },
+      {
+         id:7 ,
+         question:"The external JavaScript file must contain the <script> tag ?",
+         hints: {
+          hint1:"False",
+          hint2:"True",
+          hint3:"both",
+          hint4:"nothing"
+         },
+         Ans:"True"
+       }, 
+       {
+          id:8,
+          question:"How do you find the number with the highest value of x and y?",
+          hints: {
+            hint1:"top(x,y)",
+            hint2:"Math.ceil(x,y)",
+            hint3:"Math.Max(x,y)",
+            hint4:"Math.max(x,y)"
+          },
+          Ans:"Math.max(x,y)"
+        }
+    ]
